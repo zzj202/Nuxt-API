afce2c10ba5d1d825b2e24da543791a5b5c3a5cb
zWesley <1127394608@qq.com>
2023-06-24 17:58:00 +0800

    o1

---
+++ server/api/completions.ts
@@ -0,0 +1,23 @@
+import {H3Event, RequestHeaders} from "h3";
+import {CreateChatCompletionRequest} from "openai";
+
+export default defineEventHandler(async (event) => {
+    const headers =getHeaders(event)
+    const body = (await readBody(event)) as CreateChatCompletionRequest;
+    const response = await createChatCompletion(headers, body);
+    setResStatus(event, response.status, response.statusText);
+    return response.data;
+})
+
+
+async function createChatCompletion(
+    headers: RequestHeaders,
+    body: CreateChatCompletionRequest
+) {
+    const openai = getOpenAIApiInstance("chat", headers, body);
+    return openai.createChatCompletion(body);
+}
+export function setResStatus(event: H3Event, code: number, message: string) {
+    event.node.res.statusCode = code;
+    event.node.res.statusMessage = message;
+}
