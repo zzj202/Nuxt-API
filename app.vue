782624a5d5f81d6c43266503cea7aa6c57afffb8
zWesley <1127394608@qq.com>
2023-06-24 21:27:16 +0800

    o1

--- app.vue
+++ app.vue
@@ -1,52 +1,29 @@
 <template>
   <div>
-    {{ content }}
+    <button @click="handleV1">v1</button>
+    <button @click="handleV2">v2</button>
+    <button @click="handleV3">v3</button>
   </div>
-  <button @click="handle">处理</button>
+
 
 </template>
 
 
 <script setup lang="ts">
-
-
-
-const runtimeConfig = useRuntimeConfig()
-const content = ref("")
-const handle = async () => {
-  console.log(runtimeConfig.public.apiBase)
-  const res = await $fetch('https://api.openai.com/v1/chat/completions', {
-    method: 'post',
-    headers: {
-      'Authorization': 'Bearer ' + 'sk-69T6IRXe1xLXzO2t' + 'Onl2T3BlbkFJO02e3c5YoTxeiQt66fAu',
-    },
-    body: JSON.stringify({
-      model: "gpt-3.5-turbo",
-      messages: [
-        {
-          role: "user",
-          content: '你好',
-        },
-      ],
-      stream: true,
-    })
+const handleV1 = () => {
+  $fetch('/api/v1',{
+    method: 'POST',
   })
-  console.log(res)
-  console.log(res.data)
-  console.log(res.body)
-
-  const reader = res?.getReader();
-  const textDecoder = new TextDecoder();
-  while (1) {
-    const {done, value} = await reader.read()
-    if (done) {
-      break;
-    }
-    console.log(value)
-    const str = textDecoder.decode(value)
-    console.log(str)
-    content.value += str
-  }
+}
+const handleV2 = () => {
+  $fetch('/api/v2',{
+    method: 'POST',
+  })
+}
+const handleV3 = () => {
+  $fetch('/api/v3',{
+    method: 'POST',
+  })
 }
 
 
